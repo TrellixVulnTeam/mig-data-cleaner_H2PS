81ca9342367d4045005f168786908842d8be6da6
Jeremy Parkin <djplana@gmail.com>
2022-09-23 14:12:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ if page == "1: Getting Started":
 
 
         st.session_state.df_untouched['Audience Reach'] = st.session_state.df_untouched['Audience Reach'].astype('Int64')
-                st.session_state.df_untouched['AVE'] = st.session_state.df_untouched['AVE'].fillna(0)
+        st.session_state.df_untouched['AVE'] = st.session_state.df_untouched['AVE'].fillna(0)
 
 
         st.header('Exploratory Data Analysis')
